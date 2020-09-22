dd24e61b1643f5ffdc06ecd88aebbb0b435404a4
Sander Byrkjeland <sunbyr@gmail.com>
2020-09-22 17:00:13 +0200

    done

--- script.js
+++ script.js
@@ -10,8 +10,18 @@ const games = [
 games.forEach(function (game) {
 	container.innerHTML += `
 	<div class="inner-container">
-	<h4>Name: ${game.name}</h4>
+	<h4>Name: ${game.name}</h4> 
 	<p>Rating: ${game.rating}</p>
-	<div>
-	`;
+	</div>`;
 });
+
+// You have to querySelectorAll AFTER you render the html, or else the "h4" tag does not exist yet
+const oneGame = document.querySelectorAll("h4");
+
+oneGame.forEach(function (game) {
+	game.addEventListener("click", toggleClass);
+});
+
+function toggleClass(event) {
+	event.target.classList.toggle("played");
+}
